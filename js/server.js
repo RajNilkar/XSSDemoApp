8daa89d8eb88655b1bff4f55177b18e115a39d45
RajNilkar <rajitnilkar28@gmail.com>
2025-11-25 17:28:57 -0700

    Created initial testing for three types of XSS attacks and created an express.js server for routing.

---
+++ js/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '..', 'html')));
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
