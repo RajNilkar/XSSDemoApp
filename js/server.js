acf2ee87f5cda1d93ef50682444f0976b550f2f6
RajNilkar <rajitnilkar28@gmail.com>
2025-11-30 18:23:32 -0700

    Fixed path issues for running the server and added some styles

--- js/server.js
+++ js/server.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+//Serve index.html from root directory
+app.use(express.static(path.join(__dirname, '..')));
+
+//Serve other html files
 app.use(express.static(path.join(__dirname, '..', 'html')));
+
+//Serve CSS from /css
+app.use('/css', express.static(path.join(__dirname, '..', 'css')));
+
+//Home route (index.html)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'))
+});
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
