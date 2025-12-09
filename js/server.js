4117d0c2044388b342e55b9af8933e196a16a333
RajNilkar <rajitnilkar28@gmail.com>
2025-12-09 07:13:19 -0700

    Changed some file paths to keep everything organized and fixed some code

--- js/server.js
+++ js/server.js
@@ -2,18 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-//Serve index.html from root directory
-app.use(express.static(path.join(__dirname, '..')));
+//Serve all static files from the public directory
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-//Serve other html files
-app.use(express.static(path.join(__dirname, '..', 'html')));
-
-//Serve CSS from /css
-app.use('/css', express.static(path.join(__dirname, '..', 'css')));
-
-//Home route (index.html)
+//Home route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'index.html'))
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
